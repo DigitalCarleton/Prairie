b2dd87c8c429f187e274120677d47ba260a436a8
Caitlin Donahue <caitlindonahue95@gmail.com>
2016-10-11 12:00:43 -0500

    Adding Audio clipe player

---
+++ Project/Assets/Prairie/Framework/Script/Interaction/PlayAudioInteraction.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAudioInteraction : Interaction
+{
+    public AudioClip audioClip;
+    public AudioSource audioSource;
+    public bool toggle = false;
+
+
+    protected override void PerformAction()
+    {
+        if (audioSource == null)
+        {
+             audioSource = gameObject.AddComponent<AudioSource>();
+        }
+
+        audioSource.clip = audioClip;
+        audioSource.Play();
+        
+    }
+}
