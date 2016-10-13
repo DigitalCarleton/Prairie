92de7b518de710829a36da511db00297185b8288
Caitlin Donahue <caitlindonahue95@gmail.com>
2016-10-12 19:47:46 -0500

    Update Audio Player
    Removed toggle because apparently unity does that automatically so it
    was not neccessary

--- Project/Assets/Prairie/Framework/Script/Interaction/PlayAudioInteraction.cs
+++ Project/Assets/Prairie/Framework/Script/Interaction/PlayAudioInteraction.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class PlayAudioInteraction : Interaction
 {
     public AudioClip audioClip;
+    //we have a publi audio source so that the user can import their own
     public AudioSource audioSource;
-    public bool toggle = false;
 
-
+    /// <summary>
+    /// Plays an audio clip when the player interacts with an object
+    /// </summary>
     protected override void PerformAction()
     {
+        //A default audio source is created if the user does not import one
         if (audioSource == null)
         {
              audioSource = gameObject.AddComponent<AudioSource>();
@@ -17,6 +20,7 @@ public class PlayAudioInteraction : Interaction
 
         audioSource.clip = audioClip;
         audioSource.Play();
+        
         
     }
 }
