eab6f379a5b9db47302e8a2630148f9b26d24322
Graham Earley <graham.p.earley@gmail.com>
2016-11-06 10:19:46 -0600

    Use new activate method with root interactor param

--- Project/Assets/Prairie/Framework/Script/Interaction/NextTwineNodeInteraction.cs
+++ Project/Assets/Prairie/Framework/Script/Interaction/NextTwineNodeInteraction.cs
@@ -11,7 +11,7 @@ public class NextTwineNodeInteraction : Interaction
 
 		if (twineNodeComponent != null && !twineNodeComponent.enabled && twineNodeComponent.HasActiveParentNode()) 
 		{
-			twineNodeComponent.Activate ();
+			twineNodeComponent.Activate (this.rootInteractor);
 			twineNodeComponent.DeactivateAllParents ();
 		}
 	}
