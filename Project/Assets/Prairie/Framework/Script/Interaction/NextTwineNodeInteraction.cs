61ebe2f445ff75a8c1edd4b26f882efc0b203994
Graham Earley <graham.p.earley@gmail.com>
2016-11-06 10:10:21 -0600

    Add `NextTwineNodeInteraction` for triggering next part in twine story

---
+++ Project/Assets/Prairie/Framework/Script/Interaction/NextTwineNodeInteraction.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextTwineNodeInteraction : Interaction 
+{
+	public GameObject nextTwineNode;
+
+	protected override void PerformAction () 
+	{
+		TwineNode twineNodeComponent = this.nextTwineNode.GetComponent<TwineNode> ();
+
+		if (twineNodeComponent != null && !twineNodeComponent.enabled && twineNodeComponent.HasActiveParentNode()) 
+		{
+			twineNodeComponent.Activate ();
+			twineNodeComponent.DeactivateAllParents ();
+		}
+	}
+}
+
