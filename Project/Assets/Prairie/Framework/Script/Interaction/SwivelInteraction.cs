b71a2cce06324291fefe54efd40afbeb08861883
Matt <javalym@carleton.edu>
2016-11-06 19:44:49 -0600

    Added new SwivelInteraction.

---
+++ Project/Assets/Prairie/Framework/Script/Interaction/SwivelInteraction.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwivelInteraction : Interaction
+{
+
+	public GameObject hinge;
+	private float rotateSpeed = 90.0f;
+	private float targetAngle = 0;
+	const float rotationAmount = 1.5f;
+
+	protected override void PerformAction()
+	{
+		targetAngle -= rotateSpeed;
+	}
+
+	void Update()
+	{
+		// Trigger functions if Rotate is requested
+		if (Input.GetKeyDown (KeyCode.G))
+		{
+			targetAngle += rotateSpeed;
+		}
+		if (targetAngle != 0)
+		{
+			Rotate();
+		}
+	}
+
+	protected void Rotate()
+	{
+		if (targetAngle > 0)
+		{
+			transform.RotateAround (hinge.transform.position, Vector3.up, -rotationAmount);
+			targetAngle -= rotationAmount;
+		}
+		else if (targetAngle < 0)
+		{
+			transform.RotateAround (hinge.transform.position, Vector3.up, rotationAmount);
+			targetAngle += rotationAmount;
+		}
+	}
+}
