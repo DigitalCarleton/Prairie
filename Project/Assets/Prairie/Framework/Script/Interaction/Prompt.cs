5b5283e7b60f19bac6ad377416a51209ca8bf46d
Charlie Imhoff <cpimhoff@gmail.com>
2017-01-17 23:00:57 -0600

    UpperCamelCase method names...

--- Project/Assets/Prairie/Framework/Script/Interaction/Prompt.cs
+++ Project/Assets/Prairie/Framework/Script/Interaction/Prompt.cs
@@ -6,7 +6,7 @@ public class Prompt : MonoBehaviour
 
 	public string promptText;
 	
-	public void drawPrompt()
+	public void DrawPrompt()
 	{
 		// Draw a GUI with the interaction 
 		Rect frame = new Rect (Screen.width / 2, Screen.height / 2, Screen.width / 4, Screen.height / 4); 
