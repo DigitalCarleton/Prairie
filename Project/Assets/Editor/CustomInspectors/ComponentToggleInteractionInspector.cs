d0bf8115a7f3299bb1ec2fb381868e1126c8ce13
Chris Shoemaker <shoemakerc@carleton.edu>
2017-01-31 10:55:45 -0600

    Adding ComponentToggleInteraction custom inspector using PrairieGUI.  Looks nice.

---
+++ Project/Assets/Editor/CustomInspectors/ComponentToggleInteractionInspector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ComponentToggleInteraction))]
+public class ComponentToggleInteractionInspector : Editor {
+
+	ComponentToggleInteraction componentToggle;
+
+	public override void OnInspectorGUI()
+	{
+		componentToggle = (ComponentToggleInteraction)target;
+		componentToggle.target= PrairieGUI.drawObjectList ("Behaviours To Toggle", componentToggle.target);
+	}
+}
