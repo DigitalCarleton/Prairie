1dba88268fcb027393efa510eb007a393ef3dfeb
Chris Shoemaker <shoemakerc@carleton.edu>
2017-01-30 19:52:17 -0600

    new inspector file for Slideshow.cs; current interface is very ugly

---
+++ Project/Assets/Editor/CustomInspectors/SlideshowInspector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Slideshow))]
+public class SlideshowInspector : Editor {
+
+	Slideshow slideshow;
+
+	public override void OnInspectorGUI()
+	{
+		slideshow = (Slideshow)target;
+		slideshow.Slides = PrairieGUI.drawObjectList ("Slides", slideshow.Slides);
+	}
+}
