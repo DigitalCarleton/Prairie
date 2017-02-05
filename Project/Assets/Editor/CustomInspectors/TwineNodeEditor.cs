3c706a0b3e4ab428e24630925bbdcf0e7dfee765
Brennan Kuo <brennan@eduroam-165-158.dyn.carleton.edu>
2017-02-05 00:18:13 -0600

    made fields read only

--- Project/Assets/Editor/CustomInspectors/TwineNodeEditor.cs
+++ Project/Assets/Editor/CustomInspectors/TwineNodeEditor.cs
@@ -16,12 +16,12 @@ public class TwineNodeEditor : Editor {
 
 		node.isDecisionNode = EditorGUILayout.Toggle ("Decision node?", node.isDecisionNode);
 		node.objectsToTrigger = PrairieGUI.drawObjectList ("Objects To Trigger", node.objectsToTrigger);
-		node.name = EditorGUILayout.TextField ("Name", node.name);
-		node.tags = PrairieGUI.drawPrimitiveList ("Tags", node.tags);
+		node.name = PrairieGUI.TextFieldReadOnly ("Name", node.name);
+		node.tags = PrairieGUI.drawPrimitiveListReadOnly ("Tags", node.tags);
 		node.content = EditorGUILayout.TextField ("Content", node.content);
-		node.children = PrairieGUI.drawObjectList ("Children", node.children);
+		node.children = PrairieGUI.drawObjectListReadOnly ("Children", node.children);
 		GameObject[] parentArray = node.parents.ToArray ();
-		parentArray = PrairieGUI.drawObjectList ("Parents", parentArray);
+		parentArray = PrairieGUI.drawObjectListReadOnly ("Parents", parentArray);
 		node.parents = new List<GameObject> (parentArray);
 
 		// Save changes to the TwineNode if the user edits something in the GUI:
