55521898e85a9ef50e18a17181af6278592f7e38
Chris Shoemaker <shoemakerc@carleton.edu>
2017-02-05 21:17:49 -0600

    Added warning label to AudioInteraction editor

--- Project/Assets/Editor/CustomInspectors/AudioInteractionEditor.cs
+++ Project/Assets/Editor/CustomInspectors/AudioInteractionEditor.cs
@@ -12,6 +12,15 @@ public class AudioInteractionEditor : Editor {
 		audio = (AudioInteraction)target;
 		audio.repeatable = EditorGUILayout.Toggle ("Replayable?", audio.repeatable);
 		audio.audioClip = (AudioClip)EditorGUILayout.ObjectField ("Audio Clip", audio.audioClip, typeof(AudioClip), true);
+
+		if (audio.audioClip == null) 
+		{
+			DrawWarnings ();
+		}
 	}
 
+	public void DrawWarnings()
+	{
+		PrairieGUI.warningLabel ("No audio clip attached to object.  Please add an audio clip to the slot above.");
+	}
 }
