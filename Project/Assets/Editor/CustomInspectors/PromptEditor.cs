b058d2b4d8e8882b06502efa618f874b2593c4f8
Charlie Imhoff <cpimhoff@gmail.com>
2017-02-17 16:59:54 -0600

    first pass at editor for twine prompts

--- Project/Assets/Editor/CustomInspectors/PromptEditor.cs
+++ Project/Assets/Editor/CustomInspectors/PromptEditor.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
 
 [CustomEditor(typeof(Prompt))]
 public class PromptEditor : Editor {
@@ -10,13 +12,29 @@ public class PromptEditor : Editor {
 	{
 		prompt = (Prompt)target;
 
-		GUIContent label = new GUIContent ("Prompt Text", "Text displayed when a player can interact with this object.");
-		prompt.firstPrompt = EditorGUILayout.TextField (label, prompt.firstPrompt);
+		if (prompt.isTwinePrompt)
+        {
+            TwinePromptGUI();
+        }
+        else
+        {
+            SimplePromptGUI();
+        }
 
-		if (string.IsNullOrEmpty(prompt.firstPrompt.Trim()))
-		{
+		if (GUI.changed) {
+			EditorUtility.SetDirty(prompt);
+		}
+	}
+
+    public void SimplePromptGUI()
+    {
+        GUIContent label = new GUIContent ("Prompt Text", "Text displayed when a player can interact with this object.");
+        prompt.firstPrompt = EditorGUILayout.TextField (label, prompt.firstPrompt);
+
+        if (string.IsNullOrEmpty(prompt.firstPrompt.Trim()))
+        {
             PrairieGUI.hintLabel("No prompt will be displayed in game.");
-		} else
+        } else
         {
             GUIContent cyclicLabel = new GUIContent("Cyclic Prompt", "Does this prompt have two cycling values? (i.e. open, close)");
             prompt.isCyclic = EditorGUILayout.Toggle(cyclicLabel, prompt.isCyclic);
@@ -31,9 +49,37 @@ public class PromptEditor : Editor {
                 }
             }
         }
+    }
 
-		if (GUI.changed) {
-			EditorUtility.SetDirty(prompt);
-		}
-	}
+    public void TwinePromptGUI()
+    {
+        EditorGUILayout.Label("Twine Prompts");
+
+        // get list of twine nodes we need prompts for, the keys of our dictionary
+        AssociatedTwineNodes associatedNodes = this.prompt.gameObject.GetComponent<AssociatedTwineNodes> ();
+        List<string> twineNodeNames = new List<string> ();
+        foreach (GameObject twineNodeObject in associatedNodes.associatedTwineNodeObjects)
+        {
+            TwineNode twineNode = twineNodeObject.GetComponent<TwineNode> ();
+            twineNodeNames.Add(twineNode.name);
+        }
+
+        // build dictionary and get a value for each key
+        Dictionary<string, string> twinePrompts = new Dictionary<string, string> ();
+        foreach (string nodeName in twineNodeNames)
+        {
+            string previousValue = this.prompt.twinePrompts[nodeName];
+            if (previousValue == null)
+            {
+                previousValue = ""  // initialize a missing value
+            }
+
+            GUIContent label = new GUIContent (nodeName, "Text displayed when a player can progress the story to this twine node.");
+            twinePrompts[nodeName] = EditorGUILayout.TextField(label, previousValue);
+        }
+
+        // save dictionary back to the prompt
+        this.prompt.twinePrompts = twinePrompts;
+    }
+
 }
