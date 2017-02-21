d4b79ab3852a303c789a6ec0b54553abc1c11003
Charlie Imhoff <cpimhoff@gmail.com>
2017-02-20 21:34:16 -0600

    If only one twine prompt, use simpler GUI
    outsource to our basic GUI in order to reduce code duplication
    
    use same backend dictionary to maintain twine prompt based features

--- Project/Assets/Editor/CustomInspectors/PromptEditor.cs
+++ Project/Assets/Editor/CustomInspectors/PromptEditor.cs
@@ -26,7 +26,7 @@ public class PromptEditor : Editor {
 		}
 	}
 
-    public void SimplePromptGUI()
+    public void SimplePromptGUI(bool cyclicAllowed = true)
     {
         GUIContent label = new GUIContent ("Prompt Text", "Text displayed when a player can interact with this object.");
         prompt.firstPrompt = EditorGUILayout.TextField (label, prompt.firstPrompt);
@@ -34,28 +34,26 @@ public class PromptEditor : Editor {
         if (string.IsNullOrEmpty(prompt.firstPrompt.Trim()))
         {
             PrairieGUI.hintLabel("No prompt will be displayed in game.");
+            prompt.isCyclic = false;    // don't allow for a cycle if the first prompt is empty
+            return;
         }
-		else
-        {
-            GUIContent cyclicLabel = new GUIContent("Cyclic Prompt", "Does this prompt have two cycling values? (i.e. open, close)");
-            prompt.isCyclic = EditorGUILayout.Toggle(cyclicLabel, prompt.isCyclic);
-            if (prompt.isCyclic)
-            {
+        if (!cyclicAllowed) { return; } // guard statement
 
-                GUIContent secondLabel = new GUIContent("Second Prompt", "Second prompt to display, will toggle between this and first prompt.");
-                prompt.secondPrompt = EditorGUILayout.TextField(secondLabel, prompt.secondPrompt);
-                if (string.IsNullOrEmpty(prompt.secondPrompt.Trim()))
-                {
-                    PrairieGUI.hintLabel("Second prompt will be ignored.");
-                }
+        GUIContent cyclicLabel = new GUIContent("Cyclic Prompt", "Does this prompt have two cycling values? (i.e. open, close)");
+        prompt.isCyclic = EditorGUILayout.Toggle(cyclicLabel, prompt.isCyclic);
+        if (prompt.isCyclic)
+        {
+            GUIContent secondLabel = new GUIContent("Second Prompt", "Second prompt to display, will toggle between this and first prompt.");
+            prompt.secondPrompt = EditorGUILayout.TextField(secondLabel, prompt.secondPrompt);
+            if (string.IsNullOrEmpty(prompt.secondPrompt.Trim()))
+            {
+                PrairieGUI.hintLabel("Second prompt will be ignored.");
             }
         }
     }
 
     public void TwinePromptGUI()
     {
-		EditorGUILayout.LabelField ("Twine Prompts:");
-
         // get list of twine nodes we need prompts for, the keys of our dictionary
         AssociatedTwineNodes associatedNodes = this.prompt.gameObject.GetComponent<AssociatedTwineNodes> ();
         List<string> twineNodeNames = new List<string> ();
@@ -63,6 +61,17 @@ public class PromptEditor : Editor {
         {
             TwineNode twineNode = twineNodeObject.GetComponent<TwineNode> ();
             twineNodeNames.Add(twineNode.name);
+        }
+
+        // special case: if only one twine node - use the basic input
+        if (twineNodeNames.Count == 1)
+        {
+            // use the simple prompt GUI without allowing for cycles
+            SimplePromptGUI(cyclicAllowed: false);
+            // bind the result from simple GUI back to the twine based key
+            prompt.twinePrompts.Set(twineNodeNames[0], prompt.firstPrompt);
+            // do not fall through into the dictionary input
+            return;
         }
 
         // build dictionary and get a value for each key
