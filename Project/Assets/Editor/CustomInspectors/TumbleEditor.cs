ea95caa319f4b5484b1259b373ca83b98ab15055
Charlie Imhoff <cpimhoff@gmail.com>
2017-02-23 16:09:13 -0600

    TumbleEditor

--- Project/Assets/Editor/CustomInspectors/TumbleEditor.cs
+++ Project/Assets/Editor/CustomInspectors/TumbleEditor.cs
@@ -7,16 +7,26 @@ public class TumbleEditor : Editor {
 
 	Tumble tumble;
 
+	public void Awake()
+	{
+		this.tumble = (Tumble) target;
+	}
+
 	public override void OnInspectorGUI()
 	{
-		tumble = (Tumble)target;
+		// Configuration:
 		EditorGUILayout.LabelField ("Use I, J, K and L keys to rotate object, and Escape to put it down.");
 		EditorGUILayout.LabelField ("Distance from player:");
-		tumble.distance = EditorGUILayout.Slider(tumble.distance, 0, 10);
+		float _distance = EditorGUILayout.Slider(tumble.distance, 0, 10);
+
 		EditorGUILayout.LabelField ("Rotation speed:");
-		tumble.speed = EditorGUILayout.Slider(tumble.speed, 1, 30);
+		float _speed = EditorGUILayout.Slider(tumble.speed, 1, 30);
+		
+		// Save:
 		if (GUI.changed) {
-			EditorUtility.SetDirty(tumble);
+			Undo.RecordObject(tumble, "Modify Tumble");
+			tumble.distance = _distance;
+			tumble.speed = _speed;
 		}
 	}
 }
